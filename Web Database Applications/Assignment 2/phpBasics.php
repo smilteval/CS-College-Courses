8255375c809401b1e7db2b984d89ef1b5f7c0f27
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-02-22 21:44:11 -0500

    added styling to the table

--- Web Database Applications/Assignment 2/phpBasics.php
+++ Web Database Applications/Assignment 2/phpBasics.php
@@ -2,6 +2,27 @@
 <html lang="en">
 <head>
     <title>PHP Basics</title>
+
+    <style>
+        table{
+            margin:auto;
+            background-color: lavender;
+            box-shadow: 2px 2px 10px darkslateblue;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        th{
+            padding: 15px;
+            background-color: darkslateblue;
+            color: white
+        }
+
+        td{
+            border: 1px solid darkslateblue;  
+            padding: 15px;
+        }
+    </style>
 </head>
 <body>
     <?php
