1ccceb87fea471a0e9340e33d68d4ef4251cdc33
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-03-13 23:43:00 -0500

    Added styling to the welcome page

--- Web Database Applications/Assignment 3/welcome.php
+++ Web Database Applications/Assignment 3/welcome.php
@@ -1,6 +1,25 @@
-<?php
-    //print out the name of the logged-in user
-    if(isset($_GET['username'])){
-        echo "Welcome ".$_GET['username']."!";
-    }
-?>
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" href="welcomeStyle.css" />
+    <title>Welcome</title>
+</head>
+
+<body>
+    <div class="vid-container">
+        <video class="bgvid" autoplay="autoplay" muted="muted" preload="auto" loop>
+            <source src="https://static.videezy.com/system/resources/previews/000/007/313/original/Plexus.mp4">
+        </video>
+
+        <div class="title">
+            <?php
+            if (isset($_GET['username'])) {
+                echo "<h1>Welcome <span>" . $_GET['username'] . "</span>.</h1>";
+            }
+            ?>
+        </div>
+
+    </div>
+</body>
+
+</html>
