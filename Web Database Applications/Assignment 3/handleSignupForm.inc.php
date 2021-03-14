46f55184bc0908627837ba90d0f3f2d23f8cef71
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2021-03-13 23:23:12 -0500

    Added styling to the signup form

--- Web Database Applications/Assignment 3/handleSignupForm.inc.php
+++ Web Database Applications/Assignment 3/handleSignupForm.inc.php
@@ -16,15 +16,18 @@ $usernamePattern = "/[a-zA-Z]{1}_[a-zA-Z]+$/"; //first initial_lastname
 
 if (isset($_POST["submit"])) {
 
-    //validate username to prevent malicious code
+    //validate username and password to prevent malicious code
     $username = validateStr($_POST["username"]);
     $password = validateStr($_POST["password"]);
+
+    $file = "accounts.json";
+    $accounts = file_get_contents($file); //get the contents of all accounts
+    $accounts = json_decode($accounts); //turn that content into array
 
     //handle empty fields
     if (empty($_POST["username"])) {
         $errors["username"] = "Missing username";
-    } 
-    else {
+    } else {
         //validate format
         if (!preg_match($usernamePattern, $username)) {
             $errors["username"] = "Format is incorrect. Use first initial_lastname.";
@@ -35,29 +38,15 @@ if (isset($_POST["submit"])) {
         $errors["password"] = "Missing password";
     }
 
-    //clear the form once it's submitted
+    //if there are no errors, append the account to the accounts array
     if (count($errors) == 0) {
         $success = true;
+
+        //store the new account into an array
+        $new_account = array("username" => $username, "password" => $password);
+
+        array_push($accounts, $new_account); //append new account to the accounts array
+        $accounts = json_encode($accounts); //turn the new array back into json
+        file_put_contents($file, $accounts); //place the json into the file
     }
-
-    //get the contents of accounts
-    $file = "accounts.json";
-    $accounts = file_get_contents($file);
-
-    //turn that content into array
-    $accounts = json_decode($accounts);
-
-    //store the new account into an array
-    $new_account = array("username" => $username, "password" => $password);
-
-    //append new account to the accounts array
-    array_push($accounts, $new_account);
-
-    //turn the new array back into json
-    $accounts = json_encode($accounts);
-
-    //place the json into the file
-    file_put_contents($file, $accounts);
 }
-
-?>
--- Web Database Applications/Assignment 3/signupForm.php
+++ Web Database Applications/Assignment 3/signupForm.php
@@ -5,44 +5,43 @@ include 'handleSignupForm.inc.php';
 <!DOCTYPE html>
 
 <head>
+  <link rel="stylesheet" href="signupStyle.css" />
   <title>Sign-Up</title>
-  <style>
-    .error-text {
-      color: red;
-      font-size: small;
-    }
-
-    .success-text {
-      color: green;
-    }
-  </style>
 </head>
 
 <body>
-  <form action="signupForm.php" method="POST">
-    <p>
-      <label>Username:
-        <input type="text" name="username" placeholder="Ex. f_lastname" value="<?php echo (isset($_POST['username'])) ? $_POST['username'] : '' ?>" />
+  <div class="vid-container">
+    <video class="bgvid" autoplay="autoplay" muted="muted" preload="auto" loop>
+      <source src="https://static.videezy.com/system/resources/previews/000/007/313/original/Plexus.mp4">
+    </video>
+    <div class="inner-container">
+      <video class="bgvid inner" autoplay="autoplay" muted="muted" preload="auto" loop>
+        <source src="https://static.videezy.com/system/resources/previews/000/007/313/original/Plexus.mp4">
+      </video>
+      <form action="signupForm.php" method="POST">
+        <h2> Sign Up </h2>
+        <p>
+          <input type="text" name="username" placeholder="Username (Ex. f_lastname)" value="<?php echo (isset($_POST['username'])) ? $_POST['username'] : '' ?>" />
         <p class="error-text">
           <?php echo (isset($errors['username'])) ? $errors['username'] : ''; ?>
         </p>
-      </label>
-    </p>
-    <p>
-      <label>Password:
-        <input type="password" name="password" value="<?php echo (isset($_POST['password'])) ? $_POST['password'] : '' ?>" />
+        </p>
+        <p>
+          <input type="password" name="password" placeholder="Password" value="<?php echo (isset($_POST['password'])) ? $_POST['password'] : '' ?>" />
         <p class="error-text">
           <?php echo (isset($errors['password'])) ? $errors['password'] : ''; ?>
         </p>
-      </label>
-    </p>
-    <p>
-      <input type="submit" name="submit" value="Sign-up" />
-    </p>
-    <p class="success-text">
-      <?php echo ($success? "You signed up successfully!" : ""); ?>
-    <p>
-  </form>
+        </p>
+        <p>
+          <button type="submit" name="submit"> Sign up </button>
+        </p>
+        <p class="success-text">
+          <?php echo ($success ? "You signed up successfully!" : ""); ?>
+        <p>
+        <p>Already have an account? <a href="loginForm.php"><span>Login</span></a></p>
+      </form>
+    </div>
+  </div>
 </body>
 
 </html>
